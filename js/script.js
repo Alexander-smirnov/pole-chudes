e05a0eb46e1bb0deee924e5a0de5bf000007289d
Alexander Smirnov <sandrik85@gmail.com>
2015-02-07 10:04:58 +0200

    Its not work

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function validateForm() {
+var temp = getElementsByName("submit");
+temp.onSubmit = function () {
+var x=document.forms["myForm"]["name"].value;
+var y = Math.round((Math.random() * (10 - 1) + 1));
+console.log (y);
+if (x==null || x=="")
+  {
+  alert("You must write somesing here");
+  return false;
+  } else {
+	  x = parseInt(x);
+	  if ( x < 1 || x > 10) {
+		alert("Incorrect number. Number must be greater than 1 and less than 10");
+	  } else {
+	  
+	  if (x === y) {
+		 alert("Success!"); 
+	  } else {
+		  alert("Failed!");
+	  }
+	  
+	 return false; 
+  }
+  }
+}
+}()
